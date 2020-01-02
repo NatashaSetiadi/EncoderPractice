f858ee926625cac3027aef98b155ea786d9d6819
Natasha Setiadi <natset456@gmail.com>
2020-01-01 20:11:28 -0500

    fixed subsystem error

--- src/main/java/frc/robot/subsystems/EncoderSubsystem.java
+++ src/main/java/frc/robot/subsystems/EncoderSubsystem.java
@@ -1,25 +1,35 @@
-package frc.robot.subsystems;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.VictorSP;
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpiutil.math.MathUtil;
 import frc.robot.commands.ElevatorPositionCommand;
 
-public class EncoderSubsystem{
+/**
+ * Add your docs here.
+ */
+public class EncoderSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
 
-    // create the parts of the swerve that we can actually control [Motors,
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new ElevatorPositionCommand());
+  }
+     // create the parts of the swerve that we can actually control [Motors,
     // Encoders, etc]
-    private WPI_TalonSRX elevatorMotor;
+    private WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(1);
     private Encoder encoder = new Encoder(0, 1, false,Encoder.EncodingType.k4X);
-    private double steerOffset;
+    private double steerOffset = Math.toDegrees(100);
 
     private final double Kp = 1;
     private final double Ki = 0;
@@ -27,20 +37,11 @@ public class EncoderSubsystem{
 
     PIDController pid = new PIDController(Kp, Ki, Kd);
 
-    public EncoderSubsystem(){}
-    public EncoderSubsystem(int elevatorMotor, double steerOffset){
-        this.elevatorMotor = new WPI_TalonSRX(elevatorMotor);
-        //setup steeroffset and convert it from degrees to radians
-        this.steerOffset = Math.toDegrees(steerOffset);
-
-        //enable continuous input from 0 to 2 pi radians
+    public EncoderSubsystem(){
         pid.enableContinuousInput(-180, 180);
+    }
+ 
 
-    }
-    @Override
-	public void initDefaultCommand() {
-        this.setDefaultCommand(new ElevatorPositionCommand());    
-    }
     public void motorSet(){
         elevatorMotor.set(MathUtil.clamp(pid.calculate(encoder.getDistance(), 180), -0.5, 0.5));
         System.out.println("PID Output:" + MathUtil.clamp(pid.calculate(encoder.getDistance(), 180), -0.5, 0.5));
@@ -54,5 +55,4 @@ public class EncoderSubsystem{
 	public void stop() {
 		
 	}
-    
 }
